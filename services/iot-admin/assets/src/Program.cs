5abefa71352a05dba7127288ef62ff1135996786
Elton Stoneman <elton@sixeyed.com>
2019-07-03 22:56:16 +0100

    Inject server DNS into stack file

--- services/iot-admin/assets/src/Program.cs
+++ services/iot-admin/assets/src/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using io = System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using IotStarterKit.Docker;
 
 namespace IotStarterKit
 {
@@ -13,7 +15,17 @@ namespace IotStarterKit
     {
         public static void Main(string[] args)
         {
+            InterpolateTemplatedFiles()
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void InterpolateTemplatedFiles()
+        {
+            //fancy name for a hacky method
+            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "stacks", "device.yml");
+            var stack = io.File.ReadAllText(composeFilePath);
+            stack = stack.Replace("{server-dns}", Secret.Read("server-dns"));
+            io.File.WriteAllText(composeFilePath, stack);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
