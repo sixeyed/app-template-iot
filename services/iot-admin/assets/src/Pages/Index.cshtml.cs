b13f3e7af76ad19f3a44664603a316a23f9a7275
Elton Stoneman <elton@sixeyed.com>
2019-07-03 14:09:59 +0100

    Admin site code

---
+++ services/iot-admin/assets/src/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using io = System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IotStarterKit.Pages
+{
+    public class IndexModel : PageModel
+    {        
+        public string JenkinsUrl {get; private set;}
+        public string JenkinsUsername {get; private set;}
+        public string JenkinsPassword {get; private set;}
+
+        public string GitHubUrl {get; private set;}
+
+        public IndexModel()
+        {
+            JenkinsUrl = io.File.ReadAllText("/jenkins-url");
+            JenkinsUsername = io.File.ReadAllText("/run/secrets/jenkins-username");
+            JenkinsPassword = io.File.ReadAllText("/run/secrets/jenkins-password");
+
+            var githubRepo = io.File.ReadAllText("/github-repo");
+            var githubUser = io.File.ReadAllText("/github-username");
+            GitHubUrl = $"https://github.com/{githubRepo}/{githubUser}.git";
+        }      
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ services/iot-admin/assets/src/Tasks/StackDeploy.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+
+namespace IotStarterKit
+{
+    public class StackDeploy
+    {
+        /*
+        host needs docker cli
+        call with --host etc. settings
+        bundle stacks
+        add versioning?
+         */
+    }
+}
