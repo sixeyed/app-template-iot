3211979d4f073c08d01caeaa108c463a2651a021
Elton Stoneman <elton@sixeyed.com>
2019-07-03 23:05:37 +0100

    Fix stack interpolation

--- services/iot-admin/assets/src/Pages/Index.cshtml.cs
+++ services/iot-admin/assets/src/Pages/Index.cshtml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using io = System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -15,17 +14,15 @@ namespace IotStarterKit.Pages
     public class IndexModel : PageModel
     {        
         private readonly ILogger _logger;
-        private readonly IWebHostEnvironment _hostingEnvironment;
 
         public string JenkinsUrl {get; private set;}
         public string JenkinsUsername {get; private set;}
         public string JenkinsPassword {get; private set;}
         public string GitHubUrl {get; private set;}
 
-        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            _hostingEnvironment = hostingEnvironment;
 
             JenkinsUrl = Secret.Read("jenkins-url");
             JenkinsUsername = Secret.Read("jenkins-username");
@@ -34,7 +31,18 @@ namespace IotStarterKit.Pages
             var githubRepo = Secret.Read("github-repo");
             var githubUser = Secret.Read("github-username");
             GitHubUrl = $"https://github.com/{githubUser}/{githubRepo}.git";
-        }      
+
+            InterpolateTemplatedFiles();
+        }              
+
+        private void InterpolateTemplatedFiles()
+        {
+            //fancy name for a hacky method
+            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "wwwroot", "stacks", "device.yml");
+            var stack = io.File.ReadAllText(composeFilePath);
+            stack = stack.Replace("{server-dns}", Secret.Read("server-dns"));
+            io.File.WriteAllText(composeFilePath, stack);
+        }
 
         public void OnGet()
         {
@@ -43,7 +51,7 @@ namespace IotStarterKit.Pages
         
         public void OnPostTest()
         {
-            var composeFilePath = io.Path.Combine(_hostingEnvironment.WebRootPath, "stacks", "service.yml");
+            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "wwwroot", "stacks", "service.yml");
             _logger.LogInformation($"Deploying stack from: {composeFilePath}, on: Test Server");
             var cmd = new StackDeploy(composeFilePath, Target.TestServer);
             var output = cmd.Run();
@@ -52,7 +60,7 @@ namespace IotStarterKit.Pages
 
         public void OnPostLocal()
         {           
-            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "stacks", "device.yml");
+            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "wwwroot", "stacks", "device.yml");
             _logger.LogInformation($"Deploying stack from: {composeFilePath}, on: Local Device");
             var cmd = new StackDeploy(composeFilePath, Target.LocalDevice);
             var output = cmd.Run();
--- services/iot-admin/assets/src/Program.cs
+++ services/iot-admin/assets/src/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using io = System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -15,17 +14,7 @@ namespace IotStarterKit
     {
         public static void Main(string[] args)
         {
-            InterpolateTemplatedFiles()
             CreateHostBuilder(args).Build().Run();
-        }
-
-        private static void InterpolateTemplatedFiles()
-        {
-            //fancy name for a hacky method
-            var composeFilePath = io.Path.Combine(io.Directory.GetCurrentDirectory(), "stacks", "device.yml");
-            var stack = io.File.ReadAllText(composeFilePath);
-            stack = stack.Replace("{server-dns}", Secret.Read("server-dns"));
-            io.File.WriteAllText(composeFilePath, stack);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
